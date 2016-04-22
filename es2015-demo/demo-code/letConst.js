df1b36f82700b72843cd64112d4d4babd5a6b36a
Arktronic <arktronic@gmail.com>
2016-04-22 13:46:00 -0400

    Added let-const demo code

---
+++ es2015-demo/demo-code/letConst.js
@@ -0,0 +1,24 @@
+// var is not block-scope
+var a = 1;
+if (something) {
+    var a = 2;
+}
+console.log(a); // -> 2
+var a = 3;
+
+// let is block-scope
+let b = 1;
+if (something) {
+    let b = 2;
+}
+console.log(b); // -> 1
+// can't do this:
+//let b = 3;
+
+// const is block-scope
+const c = 1;
+if (something) {
+    const c = 2;
+}
+// can't do this:
+//const c = 3;
