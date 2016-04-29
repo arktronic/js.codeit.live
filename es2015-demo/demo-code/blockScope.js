5a30cdee1321bd8962cd5b9444ee50c5e7972ec7
James W. Lane <lanejames@johndeere.com>
2016-04-29 09:08:28 -0500

    Updated the demo code for Block Scope

--- es2015-demo/demo-code/blockScope.js
+++ es2015-demo/demo-code/blockScope.js
@@ -1,9 +1,14 @@
+// Old way to Block Scope
+(function () {
+  var hw = 'Hello World!';
+}());
+console.log(hw); // Referance Error
+
 // "var": define variables in the scope of the entire function.
 // "let": define variables in the scope of a block (new in ES6)
-console.log(a); // "undefined": a is declared below, but is function-scoped!
 {
   var a = 'Hello';
   let b = 'world';
-  console.log(`${a} ${b}`); // "Hello world"
+  console.log(a + ' ' + b); // "Hello world"
 }
-console.log(`${a} ${b}`); // will throw: b is defined in the scope of the block
+console.log(a + ' ' + b); // will throw: b is defined in the scope of the block
